0df7988e9b2ed7878b66498ef9dc5389443c4b74
Elleylson santtos <maxta.gamer@gmail.com>
2023-05-10 23:44:54 -0300

    update

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const filmes = require('./src/data/filmes.json');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const alunos = require("./src/data/alunos.json");
+const users = require("./src/data/users.json");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    return res.json(filmes);
-})
+app.get("/", (req, res) => {
+  return res.json("index");
+});
+
+app.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/alunos", (req, res) => {
+  return res.json(alunos);
+});
 
 app.listen(21262, () => {
-    console.log('Express startd at http://localhost:21262');
-})
+  console.log("Express startd at http://localhost:21262");
+});
